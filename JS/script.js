806d623e1d8c00d2567c6002b391ecae188a6237
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-18 07:03:24 +0600

    Validation

--- JS/script.js
+++ JS/script.js
@@ -40,6 +40,21 @@ class UI {
         document.querySelector('#author').value = "";
         document.querySelector('#isbn').value = "";
     }
+
+    showAlert(message, className){
+        let div = document.createElement('div');
+        div.className= `alert ${className}` //Skeleton CSS Class -> alert className
+
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.container')
+        let form = document.querySelector('#book-form')
+
+        container.insertBefore(div, form) //new div boshalam form er age
+
+        setTimeout(()=>{
+            document.querySelector('.alert').remove();
+        }, 2500);
+    }
 }
 
 //Add event listener
@@ -53,13 +68,20 @@ function newBook(e) {
         author = document.querySelector('#author').value,
         isbn = document.querySelector('#isbn').value;
 
-    let book = new Book(title, author, isbn);
-
     let ui = new UI();
 
-    ui.addToBookList(book);
+    if (title === '' || author === '' || isbn === '') {
+       
+        ui.showAlert("Please Fill up all the Fields!!", "error");
 
-    ui.clearFields();
+    } else {
+        let book = new Book(title, author, isbn);
+        ui.addToBookList(book);
+        ui.clearFields();
+        ui.showAlert("Book Added!!", "success");
+    }
+
+
 
 
     //console.log("Submitted");
